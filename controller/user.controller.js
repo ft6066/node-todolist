a9a1f32175cb9b1419ca340a11788aabbc31e9f8
Seung Hwang <2018066@naver.com>
2025-07-25 21:13:12 +0900

    백엔드 회원가입, 로그인 로직 구현

---
+++ controller/user.controller.js
@@ -0,0 +1,43 @@
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+
+const userController = {};
+
+userController.createUser = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      throw new Error("이미 가입된 유저입니다.");
+    }
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    const newUser = new User({ email, name, password: hash });
+    await newUser.save();
+    res.status(200).json({ status: "성공입니다." });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ status: "실패입니다", err: { message: err.message } });
+  }
+};
+
+userController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }, "-createdAt -updatedAt -__v");
+    if (user) {
+      const isMath = bcrypt.compareSync(password, user.password);
+      if (isMath) {
+        const token = await user.generateToken();
+        return res.status(200).json({ status: "성공", user, token });
+      }
+    }
+    throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
+  } catch (err) {
+    res.status(400).json({ status: "실패", err: { message: err.message } });
+  }
+};
+
+module.exports = userController;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const express = require("express");
 const router = express.Router();
 
 const taskApi = require("./task.api");
+const userApi = require("./user.api");
 
 router.use("/tasks", taskApi);
+router.use("/user", userApi);
 
 module.exports = router;
